8f63a8323c08fcbf60c633d61b1abf8ffcbc38c4
pawel <fiderek@gmail.com>
2023-12-15 11:47:09 +0000

    tt

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0 ; i<100; i++){
+                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Hello World!");
+            }
+            
+        }
+    }
+}
